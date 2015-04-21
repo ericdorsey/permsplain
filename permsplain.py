f444729d21398eed5a1f084fe6f3e0936df8da98
Eric Dorsey <dorseye@gmail.com>
2015-04-20 20:47:48 -0600

    first commit

---
+++ permsplain.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from __future__ import print_function  # Force use print()
+import subprocess
+import sys
+
+if len(sys.argv) > 1:
+    argument = sys.argv[1]
+    #print("argument was {0}".format(argument))
+    popen_call = ["ls", "-l", str(argument)]
+else:
+    popen_call = ["ls", "-l"]
+
+#list_output = subprocess.Popen(["ls", "-l", "{0}".format(argument)], stdout=subprocess.PIPE).communicate()
+list_output = subprocess.Popen(popen_call, stdout=subprocess.PIPE).communicate()
+
+print(list_output)
+list_output = list(list_output)
+print("")
+list_output = list_output[:-1]
+print(list_output)
+print("")
+
+#list_output = list_output[:-1]
+list_output = list_output[0].split("\n")
+list_output.pop()
+print(list_output)
+#print(list_output)
+#list_output.pop()
+
+#print(list_output[1])
+# for i in list_output[:-1]:
+#     print(i)
